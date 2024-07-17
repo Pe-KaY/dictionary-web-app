1bbe2d93bd0e7691b62f7db5317a7874dc56d66d
Paa Kwesi Ephraim <peekaylink@gmail.com>
2024-07-17 01:24:04 +0000

    completed dictionary Ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +6,26 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'dictionary-web-app';
+  // darkmode boolean
+  isDarkMode: boolean = false;
+  // searchbar focused
+  isFocused: boolean = false;
+
+  // Darkmode toggle function
+  darkMode() {
+    const toggle = document.querySelector('.toggle');
+    toggle?.toggleAttribute('darkmode');
+    this.isDarkMode = !this.isDarkMode;
+  }
+
+  //search bar styling function
+  onfocus() {
+    this.isFocused = true;
+  }
+  onblur() {
+    this.isFocused = false;
+  }
 }
